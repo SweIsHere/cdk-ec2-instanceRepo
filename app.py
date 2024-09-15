3c928fef33ce5916f6dcff6fea78e7fcecafee0e
SweIsHere <angel.tribeno@utec.edu.pe>
2024-09-15 17:21:00 -0500

    pdf

--- app.py
+++ app.py
@@ -1,28 +1,11 @@
-#!/usr/bin/env python3
-import os
-
+# app.py
 import aws_cdk as cdk
-
-from cdk_ec2_project.cdk_ec2_project_stack import CdkEc2ProjectStack
-
+from lib.cdk_ec2_instance_stack import CdkEc2InstanceStack
 
 app = cdk.App()
-CdkEc2ProjectStack(app, "CdkEc2ProjectStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+CdkEc2InstanceStack(app, "CdkEc2InstanceStack",
+                    env=cdk.Environment(account="724707870327", region="us-east-1")
+                    )
 
 app.synth()
--- cdk_ec2_project/cdk_ec2_project_stack.py
+++ cdk_ec2_project/cdk_ec2_project_stack.py
@@ -1,19 +1,75 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    core,
+    aws_ec2 as ec2,
+    aws_iam as iam
 )
-from constructs import Construct
 
-class CdkEc2ProjectStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+class Ec2WebStack(core.Stack):
 
-        # The code that defines your stack goes here
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkEc2ProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Parámetros definidos
+        instance_name = core.CfnParameter(self, "InstanceName",
+                                          type="String",
+                                          default="MV Reemplazar",
+                                          description="Nombre de la instancia a crear")
+
+        ami_id = core.CfnParameter(self, "AMI",
+                                   type="String",
+                                   default="ami-0aa28dab1f2852040",
+                                   description="ID de la AMI")
+
+        # Crear el Security Group
+        vpc = ec2.Vpc.from_lookup(self, "VPC", is_default=True)  # Obtiene el VPC por defecto
+        security_group = ec2.SecurityGroup(self, "InstanceSecurityGroup",
+                                           vpc=vpc,
+                                           description="Permitir tráfico SSH y HTTP desde cualquier lugar")
+
+        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "Permitir SSH")
+        security_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "Permitir HTTP")
+
+        # Asociar el Instance Profile a la instancia EC2
+        instance = ec2.Instance(self, "EC2Instance",
+                                instance_type=ec2.InstanceType("t2.micro"),
+                                machine_image=ec2.MachineImage.generic_linux({
+                                    "us-east-1": ami_id.value_as_string
+                                }),
+                                vpc=vpc,  # Usa el VPC definido arriba
+                                key_name="vockey",  # La key_name para el acceso SSH
+                                security_group=security_group,
+                                block_devices=[
+                                    ec2.BlockDevice(
+                                        device_name="/dev/sda1",
+                                        volume=ec2.BlockDeviceVolume.ebs(20)  # Tamaño del volumen en GB
+                                    )
+                                ],
+                                # Aquí se usa el Instance Profile en lugar del rol directamente
+                                role=iam.InstanceProfile.from_instance_profile_arn(self, "InstanceProfile",
+                                                                                    "arn:aws:iam::724707870327:instance-profile/LabInstanceProfile"),
+                                user_data=ec2.UserData.custom('''
+                                    #!/bin/bash
+                                    cd /var/www/html/
+                                    git clone https://github.com/utec-cc-2024-2-test/websimple.git
+                                    git clone https://github.com/utec-cc-2024-2-test/webplantilla.git
+                                    ls -l
+                                ''')  # Script de User Data para clonar los repositorios
+                                )
+
+        # Salidas del stack
+        core.CfnOutput(self, "InstanceId",
+                       description="ID de la instancia EC2",
+                       value=instance.instance_id)
+
+        core.CfnOutput(self, "InstancePublicIP",
+                       description="IP pública de la instancia",
+                       value=instance.instance_public_ip)
+
+        core.CfnOutput(self, "WebSimpleURL",
+                       description="URL de websimple",
+                       value=f"http://{instance.instance_public_ip}/websimple")
+
+        core.CfnOutput(self, "WebPlantillaURL",
+                       description="URL de webplantilla",
+                       value=f"http://{instance.instance_public_ip}/webplantilla")
