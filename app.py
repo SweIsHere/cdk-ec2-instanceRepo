e02a2eac073ca47d1cb705674a0193a3554fea9e
SweIsHere <angel.tribeno@utec.edu.pe>
2024-09-15 17:25:12 -0500

    bkk

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import aws_cdk as cdk
 from lib.cdk_ec2_instance_stack import CdkEc2InstanceStack
 
 app = cdk.App()
-#.*-*
 CdkEc2InstanceStack(app, "CdkEc2InstanceStack",
                     env=cdk.Environment(account="724707870327", region="us-east-1")
                     )
